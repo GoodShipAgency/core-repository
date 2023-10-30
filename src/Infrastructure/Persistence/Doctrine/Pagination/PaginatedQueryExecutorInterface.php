de9bdd83f8da7ebfd6d3104bb737fe0004ff342a
kjacques <kjacques@mashbo.com>
2023-10-30 10:48:02 +0000

    Update namespacing

--- src/Infrastructure/Persistence/Doctrine/Pagination/PaginatedQueryExecutorInterface.php
+++ src/Infrastructure/Persistence/Doctrine/Pagination/PaginatedQueryExecutorInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Mashbo\CoreRepository\Infrastructure\Persistence\Doctrine;
+namespace Mashbo\CoreRepository\Infrastructure\Persistence\Doctrine\Pagination;
 
 use Doctrine\ORM\QueryBuilder;
 use Mashbo\CoreRepository\Domain\Pagination\LimitOffsetPage;
